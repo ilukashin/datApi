3f675599563472177a775c5b97e365307659d99c
gregoryawesoman <ray_couper@mail.ru>
2021-02-11 23:56:06 +0300

    added rake tasts with shedule args

---
+++ Rakefile
@@ -0,0 +1,8 @@
+
+task :daily_job do
+  ruby 'main.rb' 'daily'
+end
+
+task :hourly_job do
+  ruby 'main.rb' 'hourly'
+end
--- app.rb
+++ app.rb
@@ -4,11 +4,13 @@ require 'yaml'
 Dir[File.join('.', 'lib', '**', '*.rb')].each { |f| require f }
 
 class App
-  attr_reader :jobs_configs, :db_config
+  attr_reader :jobs_configs, :filtered_configs, :db_config
 
   def initialize
     @jobs_configs = Dir[File.join('jobs', '**', '*.yml')].map { |f| YAML.load(File.read(f)) }
     @db_config = YAML.load(File.read('./config/database.yml'))
+
+    @filtered_configs = jobs_configs.select { |config| config['shedule'].eql?(ARGV[0])
   end
 
   def run
